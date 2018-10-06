783ccc3b41ebcb6c8d589ee4d49ad8685fce59e4
Lorenzo Ramirez <lramirez5@wisc.edu>
2018-10-05 21:12:00 -0500

    shifter works :)

--- Phase1/ALU_tb.sv
+++ Phase1/ALU_tb.sv
@@ -3,9 +3,9 @@ module ALU_tb();
 //reg [3:0] opcode;
 enum bit [3:0] {ADD='h0, SUB='h1, XOR='h2, RED='h3, SLL='h4, SRA='h5, ROR='h6, PADDSB='h7,
 		LW='h8, SW='h9, LLB='ha, LHB='hb, B='hc, BR='hd, PCS='he, HLT='hf} opcode;
-reg [15:0] In1, In2;
+logic signed [15:0] In1, In2;
 
-logic [15:0] Out, ExpectedOut;
+logic signed [15:0] Out, ExpectedOut;
 logic [2:0] flags;
 
 ALU iDUT(.Opcode(opcode), .Input1(In1), .Input2(In2), .Output(Out), .flagsIn(flags), .flagsOut(flags));
@@ -13,6 +13,7 @@ ALU iDUT(.Opcode(opcode), .Input1(In1), .Input2(In2), .Output(Out), .flagsIn(fla
 integer a, b, c;
 initial begin
 	// Testing boundary conditions for all operations
+/*
 	for(c = 0; c < 5'h10; c = c + 1) begin
 		$cast(opcode, c);
 		In1 = 16'h8000;
@@ -40,21 +41,40 @@ initial begin
 		In2 = 16'h7fff;
 		#5;
 	end
+*/
 
+	repeat(150) begin
+		Set_Random_Inputs;
+		#5;
+		Check_Shifter_Result;
+	end
+	
+
+/*
 	// Exhaustive test
-	for(a = 0; a < 17'h10000; a = a + 1) begin
-		In2 = a;
-		for(b = 0; b < 17'h10000; b = b + 1) begin
-			In1 = b;
-			for(c = 0; c < 5'h10; c = c + 1) begin
+	for(c = 0; c < 5'h10; c = c + 1) begin // opcode
+		for(a = 0; a < 17'h10000; a = a + 1) begin // input1
+			In2 = a;
+			for(b = 0; b < 17'h10000; b = b + 1) begin // input2
+				In1 = b;	
 				$cast(opcode, c);
 				#5;
 				if (opcode == RED) Check_RED_Result;
+				if (opcode == SLL || opcode == SRA || opcode == ROR) Check_Shifter_Result;
 			end
 		end
 	end
+*/
 	$stop();
 end
+
+integer random;
+task Set_Random_Inputs;
+random = ($random % 3);
+opcode = (random == 0) ? SLL : (random == 1) ? SRA : (random == 2) ? ROR : ROR;
+In1 = $random;
+In2 = $random;
+endtask
 
 task Check_RED_Result;
 
@@ -66,6 +86,43 @@ ExpectedOut = { {7{temp3[8]}}, temp3};
 
 if (ExpectedOut != Out) begin
 	$display("INCORRECT - Op=%h, In1=%h, In2=%h, Out=%h, ExpectedOut=%h", opcode, In1, In2, Out, ExpectedOut);
+end
+
+endtask
+
+
+task Check_Shifter_Result;
+
+logic signed [15:0] ExpectedOut_SLL, ExpectedOut_SRA, ExpectedOut_ROR;
+logic [3:0] rotate_amount, shift_amount;
+
+// Compute value for rotate right if necessary
+if (opcode == ROR) begin
+	rotate_amount = In2[3:0];
+	ExpectedOut_ROR = In1; // original value before rotating
+
+	// 1-bit rotate however many times are necessary
+	repeat(rotate_amount) ExpectedOut_ROR = {ExpectedOut_ROR[0], ExpectedOut_ROR[15:1]};
+end
+
+if (opcode == SRA) begin
+	shift_amount = In2[3:0];
+	ExpectedOut_SRA = In1; // original value before rotating
+
+	// 1-bit rotate however many times are necessary
+	repeat(shift_amount) ExpectedOut_SRA = {ExpectedOut_SRA[15], ExpectedOut_SRA[15:1]};
+end
+
+ExpectedOut_SLL = In1 << In2[3:0];
+//ExpectedOut_SRA = In2 >>> In2[3:0]; // apparently MUXing 'ExpectedOut' will force the SRA to be unsigned
+
+ExpectedOut = (opcode == SLL) ? ExpectedOut_SLL :
+		(opcode == SRA) ? ExpectedOut_SRA :
+		(opcode == ROR) ? ExpectedOut_ROR  :
+		16'hzzzz;
+
+if (ExpectedOut != Out) begin
+	$display("INCORRECT - Op=%h, In1=%h, Imm=%h, Out=%h, ExpectedOut=%h", opcode, In1, In2[3:0], Out, ExpectedOut);
 end
 
 endtask
