6c1731680679bede7eca2fa7bbc077ea8128ba79
Aaron Levin <awlevin@comcast.net>
2018-10-10 21:37:40 -0500

    started integrating PC_control module to CPU

--- Phase1/cpu.v
+++ Phase1/cpu.v
@@ -17,7 +17,7 @@ reg [15:0] dstData;
 // ALU Vars
 reg [15:0] aluIn1, aluIn2;
 wire [15:0] aluOut;
-wire [2:0] aluFlagsOut;
+wire [2:0] aluFlagsOut; // FLAGS ==>> (N, Z, V)
 reg [3:0] aluOp;
 reg [2:0] aluFlagsIn;
 reg [2:0] aluFlags;
@@ -31,12 +31,11 @@ reg [15:0] address, memDataIn;
 wire [15:0] memDataOut;
 reg dataWr, dataEnable;
 
-//Control Instr's
+// Control Instr's
 reg [15:0] nextPC;
 reg takeBranch;
-wire [15:0] instr,pc_plus_four_output,pc_plus_two_output,br_output; // bits [15:12] are the opcode
-adder_16bit pc_plus_two    (.A(pc), .B(16'h0002), .Sub(1'b0), .Sum(pc_plus_two_output), .Zero(), .Ovfl(), .Sign()); // will always have the current PC + 2
-adder_16bit branch_output  (.A(pc_plus_two_output), .B(immediate), .Sub(1'b0), .Sum(br_output), .Zero(), .Ovfl(), .Sign()); // will always have the current PC + 2
+wire [15:0] instr; // bits [15:12] are the opcode
+
 
 /////////////////////
 //     Modules	   //
@@ -52,55 +51,8 @@ RegisterFile regFile(.clk(clk), .rst(rst_n), .SrcReg1(srcReg1), .SrcReg2(srcReg2
 // ALU Module
 ALU alu(.Opcode(aluOp), .Input1(aluIn1), .Input2(aluIn2), .Output(aluOut), .flagsIn(aluFlagsIn), .flagsOut(aluFlagsOut));
 
-
-// Update or reset PC if necessary
-assign pc = (~rst_n) ? 16'h0000 : nextPC; // rst_n must be 0 for an entire clk cycle for reset to occur
-
-always @(*) 
-casex(instr[11:9])
-//adderZFlag, adderVFlag, adderNFlag};
-	//Not Equal(Z=0)
-	3'b000:
-	begin
-		takeBranch =  ~aluFlagsOut[2];
-	end
-	//Equal(Z=1)
-	3'b001:
-	begin
-		takeBranch =  aluFlagsOut[2];
-	end
-	//Greater Than(N=1)
-	3'b010:
-	begin
-		takeBranch =  aluFlagsOut[0];
-	end
-	//Less Than(Z=N=0)
-	3'b011:
-	begin
-		takeBranch =  aluFlagsOut[1] & aluFlagsOut[0];
-	end
-	//Greater Than or Equal (Z=1 or Z=N=0)
-	3'b100:
-	begin
-		takeBranch =  aluFlagsOut[2] | (~aluFlagsOut[2] & ~aluFlagsOut[0]);
-	end
-	//Less Than or Equal (N=1 or Z=1)
-	3'b101:
-	begin
-		takeBranch =  aluFlagsOut[2] | aluFlagsOut[0];
-	end
-	//Overflow(V=1)
-	3'b110:
-	begin
-		takeBranch =  aluFlagsOut[1];
-	end
-	//Unconditional
-	default
-	begin 
-		takeBranch = 1'b1;
-	end
-
-endcase
+// PC Control Module
+PC_control(.C(ccc), .I(pc_imm), .F(aluFlagsOut), .PC_in(pc_in), .PC_out(pc));
 
 always @(*) 
 casex(instr[15:12])
@@ -109,6 +61,7 @@ casex(instr[15:12])
 	4'b00xx,4'b0111 : 
 	begin 
 		// Parse the instruction, set RegisterFile to read correctly
+		pc_in = pc;
 		dstReg = instr[11:8]; 
 		srcReg1 = instr[7:4]; 
 		srcReg2 = instr[3:0];
@@ -117,14 +70,14 @@ casex(instr[15:12])
 		aluIn1 = srcData1;
 		aluIn2 = srcData2;
 		dstData = aluOut;
-		writeReg=1; 
-		nextPC= pc_plus_two_output;
+		writeReg = 1'b1; 
 	end
 
 	/*SLL SRA ROR */	
 	4'b0100,4'b0101,4'b0110 : 
 	begin 
 		// Parse the instruction, set RegisterFile to read correctly
+		pc_in = pc;
 		dstReg = instr[11:8]; 
 		srcReg1 = instr[7:4]; 
 		// Set inputs for ALU based on RegisterFile outputs 
@@ -132,13 +85,13 @@ casex(instr[15:12])
 		aluIn1 = srcData1;
 		aluIn2 = instr[3:0];
 		dstData = aluOut;
-		writeReg=1; 
-		nextPC = pc_plus_two_output;
+		writeReg = 1'b1; 
 	end
 
 	/* LW SW */	
 	4'b100x : 
 	begin
+		pc_in = pc;
 		// Parse instruction
 		dstReg = instr[11:8];
 		srcReg1 = instr[7:4]; // add data in this register to immediate offset
@@ -157,11 +110,11 @@ casex(instr[15:12])
 		dataWr = instr[8]; //LW = 4'b1000 SW = 4'b10001, so last bit of the instruction corresponds to the write data
 
 		writeReg = ~instr[8]; // Write the data to the destination register, with opposite logic to dataWr
-		nextPC = pc_plus_two_output;
 	end 
 	/* LLB LHB */
 	4'b101x : 
 	begin  
+		pc_in = pc;
 		// Parse instruction
 		dstReg = instr[11:8];
 		srcReg1 = instr[11:8]; // add data in this register to immediate offset
@@ -169,19 +122,24 @@ casex(instr[15:12])
 			{ instr[7:0], {8{1'b1}}} : //LHB
 			{ {8{1'b1}},  instr[7:0]}; //LLB
 		dstData = srcData1 & immediate;
-		nextPC = pc_plus_two_output;
-
 	end 
-	/* B BR*/
-	4'b110x : 
+
+	/* B */
+	4'b1100 : 
 	begin  
-		
-		immediate = {{6{instr[8]}}, instr[8:0],{1{1'b0}}};
-		srcReg1 = instr[7:4];
-		nextPC = takeBranch ? 
-			(instr[8]? srcData1 : br_output) : //For B use brOutput and for BR use the value in the given register
-			pc_plus_two_output;
+		pc_in = pc;
+		pc_imm = instr[8:0];
 	end
+
+	/* BR */
+	4'b1101 :
+	begin
+		pc_in = pc;
+		srcReg1 = instr[7:4];
+		pc_in = srcData1;
+		pc_imm = 9'd0;
+	end
+
 	4'b1110 : 
 	begin  
 		dstReg = instr[11:8];
