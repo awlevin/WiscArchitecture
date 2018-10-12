b50b31f9a7aee12966e3fd29fbaaf6798f888d93
unknown <lmiller36@wisc.edu>
2018-10-12 13:42:01 -0500

    Added BR logic into PC_Control & Test Bench

--- Phase1/PC_control.v
+++ Phase1/PC_control.v
@@ -1,24 +1,29 @@
 
-module PC_control(C, I, F, PC_in, PC_out);
+module PC_control(C, I, F, PC_in, BR, PC_out);
 
-input [2:0] C; // ccc - condition encodings
-input [8:0] I; // immediate offset
-input [2:0] F; // flags (N, Z, V)
-input [15:0] PC_in; 
-output [15:0] PC_out;
+input  [2:0] C; 	// ccc - condition encodings
+input  [8:0] I; 	// immediate offset
+input  [2:0] F; 	// flags (N, Z, V)
+input  [15:0] PC_in; 	// current PC Value
+input  [15:0] BR;	// value to branch to, LSB is used to signify BR vs B: (0 - BR, 1 - B)
+output [15:0] PC_out;	// Output of PC control
 
 reg takeBranch; // 1 if we should branch, 0 if we should increment PC by 2
-wire [15:0] PC_plus_2, PC_plus_2_imm; // PC_plus_2 = PC + 2, PC_plus_2_imm = PC + 2 + imm
+wire [15:0] PC_plus_2, PC_plus_2_imm,Imm_Shftd_Sign_Ext; // PC_plus_2 = PC + 2, PC_plus_2_imm = PC + 2 + imm
 wire Z_flag, V_flag, N_flag;
 
-adder_16bit add_2_module(.A(PC_in), .B(16'd2), .Sub(1'b0), .Sum(PC_plus_2), .Zero(), .Ovfl(), .Sign());
-adder_16bit add_imm_module(.A(PC_plus_2), .B({{7{I[8]}}, I}), .Sub(1'b0), .Sum(PC_plus_2_imm), .Zero(), .Ovfl(), .Sign());
+adder_16bit add_2_module(.A(PC_in), .B(16'h0002), .Sub(1'b0), .Sum(PC_plus_2), .Zero(), .Ovfl(), .Sign());
+adder_16bit add_imm_module(.A(PC_plus_2), .B(Imm_Shftd_Sign_Ext), .Sub(1'b0), .Sum(PC_plus_2_imm), .Zero(), .Ovfl(), .Sign());
 
-assign PC_out = (takeBranch) ? PC_plus_2_imm : PC_plus_2;
+assign PC_out = (takeBranch) ? 
+			((BR[0]) ? PC_plus_2_imm : BR): //BR vs B if branch is taken
+		 PC_plus_2; // Branch is not taken
 
 assign Z_flag = F[2];
 assign V_flag = F[1];
 assign N_flag = F[0];
+
+assign Imm_Shftd_Sign_Ext = {{6{I[8]}},I,1'b0};
 
 always @(*) begin
 case(C)
--- Phase1/PC_control_tb.sv
+++ Phase1/PC_control_tb.sv
@@ -1,20 +1,26 @@
 
 module PC_control_tb();
 
-logic [2:0] C_tb; // ccc - condition encodings
-logic [8:0] I_tb; // immediate offset
-logic [2:0] F_tb; // flags (Z, V, N)
-logic [15:0] PC_in_tb; 
-logic [15:0] PC_out_tb;
+logic [2:0] C_dut; // ccc - condition encodings
+logic [8:0] I_dut; // immediate offset
+logic [2:0] F_dut; // flags (Z, V, N)
+logic [15:0] PC_in_dut; 
+logic [15:0] BR_dut;
+logic [15:0] PC_out_dut;
 
 logic clk;
+
+logic Z_flag, V_flag, N_flag;
 
 enum logic [2:0] {Not_Equal=3'h0, Equal=3'h1, Greater_Than=3'h2, 
 		Less_Than=3'h3, Greater_Than_Or_Equal=3'h4,
 		Less_Than_Or_Equal=3'h5, Overflow=3'h6, 
 		Unconditional=3'h7} ccc;
 
+//Test Bench variables
 logic [8:0] imm_tb; 
+logic [15:0] PC_tb;
+logic [15:0] BR_tb;
 
 // ccc	condition
 // 000	Not_Equal (Z = 0)
@@ -26,66 +32,110 @@ logic [8:0] imm_tb;
 // 110	Overflow (V = 1)
 // 111	Unconditional
 
-PC_control iDUT(.C(C_tb), .I(I_tb), .F(F_tb), .PC_in(PC_in_tb), .PC_out(PC_out_tb));
+PC_control iDUT(.C(C_dut), .I(I_dut), .F(F_dut), .PC_in(PC_in_dut), .PC_out(PC_out_dut), .BR(BR_dut));
 
 initial begin
 clk = 0;
 
 imm_tb = 9'h004;
+PC_tb = 16'h0000;
+BR_tb = 16'hFF00;
 
 // CORRECT TESTS (when branch SHOULD be taken for each condition code)
-PC_Test(Not_Equal, imm_tb, 3'b000, 16'd0); // Z = 0
-PC_Test(Equal, imm_tb, 3'b010, 16'd0); // Z = 1 
-PC_Test(Greater_Than, imm_tb, 3'b011, 16'd0); // Z = N = 1
-PC_Test(Less_Than, imm_tb, 3'b001, 16'd0); // N = 1
-PC_Test(Greater_Than_Or_Equal, imm_tb, 3'b010, 16'd0); // Z = 1
-PC_Test(Greater_Than_Or_Equal, imm_tb, 3'b000, 16'd0); // Z = N = 0
-PC_Test(Less_Than_Or_Equal, imm_tb, 3'b001, 16'd0); //  N = 1
-PC_Test(Less_Than_Or_Equal, imm_tb, 3'b010, 16'd0); //  Z = 1
-PC_Test(Overflow, imm_tb, 3'b100, 16'd0); // V = 1
-PC_Test(Unconditional, imm_tb, 3'b000, 16'd0);
+PC_Test(Not_Equal,imm_tb, PC_tb, BR_tb); // Z = 0
+PC_Test(Equal, imm_tb, PC_tb, BR_tb); // Z = 1 
+PC_Test(Greater_Than, imm_tb, PC_tb, BR_tb); // Z = N = 1
+PC_Test(Less_Than, imm_tb, PC_tb, BR_tb); // N = 1
+PC_Test(Greater_Than_Or_Equal, imm_tb, PC_tb, BR_tb); // Z = 1
+PC_Test(Greater_Than_Or_Equal, imm_tb, PC_tb, BR_tb); // Z = N = 0
+PC_Test(Less_Than_Or_Equal, imm_tb, PC_tb, BR_tb); //  N = 1
+PC_Test(Less_Than_Or_Equal, imm_tb, PC_tb, BR_tb); //  Z = 1
+PC_Test(Overflow, imm_tb, PC_tb, BR_tb); // V = 1
+PC_Test(Unconditional, imm_tb, PC_tb, BR_tb);
 
 $stop;
 
 end
-
-
-logic Z_flag, V_flag, N_flag;
-task PC_Test(logic [2:0] C, logic [8:0] I, logic [2:0] F, logic [15:0] PC_in);
+/** Checks PC against expected value
+input  [2:0] C; 	// ccc - condition encodings
+input  [8:0] I; 	// immediate offset
+input  [15:0] PC_in; 	// current PC Value
+input  [15:0] BR;	// value to branch to, LSB is used to signify BR vs B: (0 - BR, 1 - B)
+**/
+task PC_Test (logic [2:0] C, logic [8:0] I, logic [15:0] PC_in, logic [15:0] BR_tb);
 
 logic [15:0] Expected_PC_out;
 logic takeBranch;
 
-@(posedge clk) begin
+logic [2:0] Flags;
+logic [15:0] signExtendedShiftedOffset;
 
-	C_tb = C;
-	I_tb = I;
-	F_tb = F;
-	PC_in_tb = PC_in;
+logic correct;
 
-	Z_flag = F[2];
-	V_flag = F[1];
-	N_flag = F[0];
+signExtendedShiftedOffset = {{6{I[8]}}, I[8:0],1'b0};
+
+correct=1;
+Flags = 3'h0;
+
+repeat(8) begin
+
+@(posedge clk);
+	C_dut = C;
+	I_dut = I;
+	F_dut = Flags;
+	PC_in_dut = PC_in;
+	BR_dut = 16'hFFFF;
+
+	Z_flag = Flags[2];
+	V_flag = Flags[1];
+	N_flag = Flags[0];
 	
 	takeBranch = ((C === 3'b000) && (~Z_flag)) ||
 		     ((C === 3'b001) && (Z_flag)) ||
-		     ((C === 3'b010) && (Z_flag) && (N_flag)) ||
+		     ((C === 3'b010) && (~Z_flag) && (~N_flag)) ||
 		     ((C === 3'b011) && (N_flag)) ||
 		     ((C === 3'b100) && (Z_flag || (~Z_flag && ~N_flag))) ||
 		     ((C === 3'b101) && (N_flag || Z_flag)) ||
 		     ((C === 3'b110) && (V_flag)) ||
 		     (C === 3'b111);
-	Expected_PC_out = (takeBranch) ? PC_in + {{7{I[8]}}, I} + 2: PC_in + 2;
+	Expected_PC_out = (takeBranch) ? PC_in + signExtendedShiftedOffset + 2: PC_in + 2;
 
-end
+//B insructions
 
 @(negedge clk) begin
-	if (PC_out_tb !== Expected_PC_out) $write("INCORRECT - ");
-	else $write("CORRECT - ");
-
-	$display("PC_out=%h, Expected_PC_out=%h, ccc=%b, N_flag=%b, Z_flag=%b, V_flag=%b", PC_out_tb, Expected_PC_out, C, N_flag, Z_flag, V_flag);
+	if (PC_out_dut !== Expected_PC_out) begin
+		$display("INCORECCT: \t PC_out=%h, Expected_PC_out=%h, ccc=%b, flags=%b, branch_value=%h, takeBranch:%b", PC_out_dut, Expected_PC_out, C, Flags,BR_dut,takeBranch);
+		correct = 0;
+	end	
+	else 
+		correct = 1;
 end
 
+//BR instructions
+
+@(posedge clk) BR_dut = BR_tb;
+
+	takeBranch = ((C === 3'b000) && (~Z_flag)) ||
+		     ((C === 3'b001) && (Z_flag)) ||
+		     ((C === 3'b010) && (~Z_flag) && (~N_flag)) ||
+		     ((C === 3'b011) && (N_flag)) ||
+		     ((C === 3'b100) && (Z_flag || (~Z_flag && ~N_flag))) ||
+		     ((C === 3'b101) && (N_flag || Z_flag)) ||
+		     ((C === 3'b110) && (V_flag)) ||
+		     (C === 3'b111);
+
+Expected_PC_out = (takeBranch) ? BR_tb : PC_in + 2;
+@(negedge clk) begin
+	if (PC_out_dut !== Expected_PC_out) begin
+		$display("INCORECCT: \t PC_out=%h, Expected_PC_out=%h, ccc=%b, flags=%b, branch_value=%h, takeBranch:%b", PC_out_dut, Expected_PC_out, C, Flags,BR_dut,takeBranch);
+		correct = 0;
+	end	
+	else 
+		correct = 1;
+end
+
+Flags = Flags + 1;
+end
 endtask
 
 always #5 clk = ~clk;
