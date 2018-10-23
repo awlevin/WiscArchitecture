349f41e1fd2d292e90a753dbc1cf5fe34b82a7e3
Lorne <lmiller36@wisc.edu>
2018-10-23 13:59:22 -0500

    Initial commit phase 2

---
+++ Phase2/PipelineStages.sv
@@ -0,0 +1,26 @@
+module PipelineStages(clk,rst_n);
+
+endmodule
+
+module Fetch();
+
+endmodule
+
+module Decode();
+
+endmodule
+
+module Execute();
+
+endmodule
+
+module Memory();
+
+endmodule
+
+module WriteBack();
+
+endmodule
+
+
+
