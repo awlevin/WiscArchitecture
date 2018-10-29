08e8f7f1668e244c02aadf8b5d226d382b9d0163
Lorne <lmiller36@wisc.edu>
2018-10-29 17:21:14 -0500

    New Files (and rename of TB)

---
+++ Phase2/PipelineStages.sv
@@ -0,0 +1,11 @@
+module EX_Register();//(clk,rst_n,ALUSrc_in,ALUOp_in,ALUSrc_out,ALUOp_out);
+//input clk,rst_n
+endmodule
+
+module M_Register();
+
+endmodule
+
+module WB_Register();
+
+endmodule
---
+++ Phase2/dff_4bit.v
@@ -0,0 +1,12 @@
+module dff_4bit(clk, rst, q, d, wen);
+
+input wen, clk, rst;
+input [3:0] d;
+output [3:0] q;
+
+dff	b3(.q(q[3]), .d(d[3]), .wen(wen), .clk(clk), .rst(rst)),
+    	b2(.q(q[2]), .d(d[2]), .wen(wen), .clk(clk), .rst(rst)),
+    	b1(.q(q[1]), .d(d[1]), .wen(wen), .clk(clk), .rst(rst)),
+    	b0(.q(q[0]), .d(d[0]), .wen(wen), .clk(clk), .rst(rst));
+
+endmodule
