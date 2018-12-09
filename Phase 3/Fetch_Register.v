80cc04e32bac80b0cbbf21f2fe92dc624a621b65
lramirez5 <lramirez5@wisc.edu>
2018-12-09 11:20:12 -0600

    Clarifications to Module execution

--- Phase 3/Fetch_Register.v
+++ Phase 3/Fetch_Register.v
@@ -6,7 +6,7 @@ input clk, rst_n;
 input [15:0] pc_add_in, instr_in;
 output [15:0] pc_add_out, instr_out;
 
-dff_16bit pc_add(.clk(clk), .rst(~rst_n), .q(pc_add_out), .d(pc_add_in), .wen(1'b1));
-dff_16bit instr(.clk(clk), .rst(~rst_n), .q(instr_out), .d(instr_in), .wen(1'b1));
+dff_16bit pc_add(.clk(clk), .rst(~rst_n), .q(pc_add_out), .d(pc_add_in), .wen(1'b1)); 	//PC + 2
+dff_16bit instr(.clk(clk), .rst(~rst_n), .q(instr_out), .d(instr_in), .wen(1'b1));	//Stored instruction value
 
 endmodule
